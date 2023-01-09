3a6542d1382b4833226cfa55f451e668d8da21e3
idelfatkhullin <maximbravin@gmail.com>
2023-01-09 17:48:50 +0600

    added test for saucelab

--- src/test/java/fatk/idelka/mobile/tests/local/LocalAndroidTests.java
+++ src/test/java/fatk/idelka/mobile/tests/local/LocalAndroidTests.java
@@ -1,8 +1,13 @@
 package fatk.idelka.mobile.tests.local;
 
+import com.codeborne.selenide.Command;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,6 +18,7 @@ public class LocalAndroidTests extends LocalTestBase {
     @Test
     void checkOpenedArticleIsCorrect(){
         step("Types search article", () -> {
+//            Configuration.timeout = 60000;
             $(AppiumBy.accessibilityId("cart badge")).click();
             $(AppiumBy.accessibilityId("Go Shopping button")).shouldBe(visible);
         });
